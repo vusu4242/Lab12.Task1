20c54142e3a93fcdaeb1f18b841d030c984776f0
Juliana Vus <155918257+vusu4242@users.noreply.github.com>
2025-11-27 13:20:25 +0200

    Update ATMTest.java

--- src/test/java/ua/ucu/apps/ATMTest.java
+++ src/test/java/ua/ucu/apps/ATMTest.java
@@ -1,64 +1,12 @@
 package ua.ucu.apps;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ATMTest {
-    private ATM atm;
-
-    @BeforeEach
-    void setUp() {
-        atm = new ATM();
-    }
 
     @Test
-    void testProcess1000() {
-        assertDoesNotThrow(() -> atm.process(1000));
-    }
-
-    @Test
-    void testProcess2200() {
-        assertDoesNotThrow(() -> atm.process(2200));
-    }
-
-    @Test
-    void testProcess500() {
-        assertDoesNotThrow(() -> atm.process(500));
-    }
-
-    @Test
-    void testProcess1765() {
-        assertDoesNotThrow(() -> atm.process(1765));
-    }
-
-    @Test
-    void testProcess100() {
-        assertDoesNotThrow(() -> atm.process(100));
-    }
-
-    @Test
-    void testProcess9999() {
-        assertDoesNotThrow(() -> atm.process(9999));
-    }
-
-    @Test
-    void testProcess1() {
-        assertDoesNotThrow(() -> atm.process(1));
-    }
-
-    @Test
-    void testProcess777() {
-        assertDoesNotThrow(() -> atm.process(777));
-    }
-
-    @Test
-    void testProcess3456() {
-        assertDoesNotThrow(() -> atm.process(3456));
-    }
-
-    @Test
-    void testProcess50() {
-        assertDoesNotThrow(() -> atm.process(50));
+    void testATM() {
+        ATM atm = new ATM();
+        atm.process(2200);
     }
 }
